064f461d96267f98a293890aae675556c3d2c1d5
Łukasz Florczak <lukasz.florczak@cepixel.com>
2017-06-20 19:57:58 +0200

    Clear doc with playground

---
+++ docs/App.vue
@@ -0,0 +1,74 @@
+<template>
+    <main class="main">
+
+        <div class="container">
+
+            <agile>
+                <div class="slide slide--1">slide 1</div>
+                <div class="slide slide--2">slide 2</div>
+                <div class="slide slide--3">slide 3</div>
+                <div class="slide slide--4">slide 4</div>
+                <div class="slide slide--5">slide 5</div>
+                <div class="slide slide--6">slide 6</div>
+            </agile>
+
+        </div>
+        <!-- /.container -->
+    </main>
+    <!-- /.main -->
+</template>
+
+<script>
+    import Agile from '../src/Agile'
+
+    export default {
+        name: 'app',
+        components: {Agile},
+        data () {
+            return {
+                msg: 'vue-agile'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" type="text/scss">
+
+    @import './scss/main.scss';
+
+    .slide {
+        align-items: center;
+        color: #fff;
+        display: flex;
+        font-size: 20px;
+        height: 200px;
+        justify-content: center;
+        padding: 20px;
+        width: 100%;
+
+        &--1 {
+            background-color: #f1c40f;
+        }
+
+        &--2 {
+            background-color: #e67e22;
+        }
+
+        &--3 {
+            background-color: #e74c3c;
+        }
+
+        &--4 {
+            background-color: #3498db;
+        }
+
+        &--5 {
+            background-color: #9b59b6;
+        }
+
+        &--6 {
+            background-color: #2ecc71;
+        }
+    }
+
+</style>
--- docs/main.js
+++ docs/main.js
@@ -1,46 +1,11 @@
 import Vue from 'vue'
+import App from './App'
 
-require('./docs.scss')
-
-function throttle (callback, limit) {
-    var wait = false
-    return function () {
-        if (!wait) {
-            callback.call()
-            wait = true
-            setTimeout(function () {
-                wait = false
-            }, limit)
-        }
-    }
-}
-
-const SL = ', 100%, 85%'
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    data () {
-        return {
-            isNavSticky: false,
-            firstColor: Math.floor(Math.random() * 255),
-            secondColor: Math.floor(Math.random() * 255)
-        }
-    },
-    computed: {
-        gradient () {
-            return {
-                background: `linear-gradient(to left bottom, hsl(${this.firstColor + SL}) 0%, hsl(${this.secondColor + SL}) 100%)`
-            }
-        }
-    },
-    methods: {
-        adjustNav () {
-            this.isNavSticky = window.scrollY > window.innerHeight
-        }
-    },
-    mounted () {
-        this.adjustNav()
-        window.addEventListener('scroll', throttle(this.adjustNav, 50))
-    }
+    template: '<App/>',
+    components: {App}
 })
