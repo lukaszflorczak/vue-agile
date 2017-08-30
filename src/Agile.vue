b3d012c59eb22c46b01ea75347ba25cd99e4b445
Łukasz Florczak <lukasz.florczak@cepixel.com>
2017-08-30 07:35:14 +0200

    Small change in listeners

--- src/Agile.vue
+++ src/Agile.vue
@@ -181,40 +181,37 @@
                 this.startAutoplay()
             }
 
-            // Listeners
-            this.$nextTick(function () {
-                // Windows resize listener
-                window.addEventListener('resize', this.getWidth)
+            // Windows resize listener
+            window.addEventListener('resize', this.getWidth)
 
-                // Get width on start
-                this.getWidth()
+            // Get width on start
+            this.getWidth()
 
-                // Mouse and touch events
-                if ('ontouchstart' in window) {
-                    this.el.track.addEventListener('touchstart', this.handleMouseDown)
-                    this.el.track.addEventListener('touchend', this.handleMouseUp)
-                    this.el.track.addEventListener('touchmove', this.handleMouseMove)
-                } else {
-                    this.el.track.addEventListener('mousedown', this.handleMouseDown)
-                    this.el.track.addEventListener('mouseup', this.handleMouseUp)
-                    this.el.track.addEventListener('mousemove', this.handleMouseMove)
+            // Mouse and touch events
+            if ('ontouchstart' in window) {
+                this.el.track.addEventListener('touchstart', this.handleMouseDown)
+                this.el.track.addEventListener('touchend', this.handleMouseUp)
+                this.el.track.addEventListener('touchmove', this.handleMouseMove)
+            } else {
+                this.el.track.addEventListener('mousedown', this.handleMouseDown)
+                this.el.track.addEventListener('mouseup', this.handleMouseUp)
+                this.el.track.addEventListener('mousemove', this.handleMouseMove)
+            }
+
+            // Autoplay
+            if (this.autoplay_) {
+                if (this.pauseOnHover_) {
+                    this.el.track.addEventListener('mouseover', this.stopAutoplay)
+                    this.el.track.addEventListener('mouseout', this.startAutoplay)
                 }
 
-                // Autoplay
-                if (this.autoplay_) {
-                    if (this.pauseOnHover_) {
-                        this.el.track.addEventListener('mouseover', this.stopAutoplay)
-                        this.el.track.addEventListener('mouseout', this.startAutoplay)
-                    }
-
-                    if (this.pauseOnDotsHover_) {
-                        for (let i = 0; i < this.slidesCount; ++i) {
-                            this.el.dots[i].addEventListener('mouseover', this.stopAutoplay)
-                            this.el.dots[i].addEventListener('mouseout', this.startAutoplay)
-                        }
+                if (this.pauseOnDotsHover_) {
+                    for (let i = 0; i < this.slidesCount; ++i) {
+                        this.el.dots[i].addEventListener('mouseover', this.stopAutoplay)
+                        this.el.dots[i].addEventListener('mouseout', this.startAutoplay)
                     }
                 }
-            })
+            }
         },
 
         beforeDestroy () {
