651b2cb5fbde4fd4891b146fbd662a3ee5d95eff
Łukasz Florczak <lukasz.florczak@cepixel.com>
2019-05-12 08:15:51 +0200

    Fix disableScroll()
    Resolve: #86

--- src/Agile.vue
+++ src/Agile.vue
@@ -409,12 +409,12 @@
 			},
 
 			handleMouseMove (e) {
-				this.disableScroll()
 				let positionX = ('ontouchstart' in window) ? e.touches[0].clientX : e.clientX
 				let positionY = ('ontouchstart' in window) ? e.touches[0].clientY : e.clientY
 				let dragDistanceX = Math.abs(positionX - this.dragStartX)
 				let dragDistanceY = Math.abs(positionY - this.dragStartY)
 				if (dragDistanceX > 3 * dragDistanceY) {
+					this.disableScroll()
 					this.dragDistance = positionX - this.dragStartX
 				}
 			},
