d28c3ab1a94b22601c046d5d45af51aba6a8aaf1
Łukasz Florczak <lukasz.florczak@cepixel.com>
2019-05-25 08:17:17 +0200

    Handlers methods mixin

---
+++ src/mixins/handlers.js
@@ -0,0 +1,49 @@
+/**
+ * Handlers methods for mouse/touch events
+ */
+const mixin = {
+	methods: {
+		handleMouseDown (e) {
+			if (!e.touches) {
+				e.preventDefault()
+			}
+			this.mouseDown = true
+			this.dragStartX = ('ontouchstart' in window) ? e.touches[0].clientX : e.clientX
+			this.dragStartY = ('ontouchstart' in window) ? e.touches[0].clientY : e.clientY
+		},
+
+		handleMouseMove (e) {
+			let positionX = ('ontouchstart' in window) ? e.touches[0].clientX : e.clientX
+			let positionY = ('ontouchstart' in window) ? e.touches[0].clientY : e.clientY
+			let dragDistanceX = Math.abs(positionX - this.dragStartX)
+			let dragDistanceY = Math.abs(positionY - this.dragStartY)
+			if (dragDistanceX > 3 * dragDistanceY) {
+				this.disableScroll()
+				this.dragDistance = positionX - this.dragStartX
+			}
+		},
+
+		handleMouseUp () {
+			this.mouseDown = false
+			this.enableScroll()
+		},
+
+		handleMouseOver (element) {
+			if (this.settings.autoplay) {
+				if ((element === 'dot' && this.settings.pauseOnDotsHover) || (element === 'track' && this.settings.pauseOnHover)) {
+					this.pauseAutoPlay = true
+				}
+			}
+		},
+
+		handleMouseOut (element) {
+			if (this.settings.autoplay) {
+				if ((element === 'dot' && this.settings.pauseOnDotsHover) || (element === 'track' && this.settings.pauseOnHover)) {
+					this.pauseAutoPlay = false
+				}
+			}
+		}
+	}
+}
+
+export default mixin
