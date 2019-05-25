7801a150f31ebd6ad94f25684bc8a0d0f2f0477d
Łukasz Florczak <lukasz.florczak@cepixel.com>
2019-05-25 08:17:27 +0200

    Helpers methods mixin

---
+++ src/mixins/helpers.js
@@ -0,0 +1,36 @@
+/**
+ * Helpers methods
+ */
+const mixin = {
+	methods: {
+		/**
+		 * Convert HTML Collection to JS Array
+		 */
+		htmlCollectionToArray (collection) {
+			return Array.prototype.slice.call(collection, 0)
+		},
+
+		/**
+		 * Compare elements for breakpoints sorting
+		 */
+		compare (a, b) {
+			if (a.breakpoint < b.breakpoint) {
+				return (this.initialSettings.mobileFirst) ? -1 : 1
+			} else if (a.breakpoint > b.breakpoint) {
+				return (this.initialSettings.mobileFirst) ? 1 : -1
+			} else {
+				return 0
+			}
+		},
+
+		/**
+		 * Set window & container width
+		 */
+		getWidth () {
+			this.widthWindow = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+			this.widthContainer = this.$refs.list.clientWidth
+		}
+	}
+}
+
+export default mixin
