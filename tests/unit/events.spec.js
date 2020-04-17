8cb069554c674f896f3de322f7b779b6bec4cabd
Łukasz Florczak <lukasz@florczak.dev>
2020-04-17 20:58:05 +0200

    Test fixes

--- tests/unit/events.spec.js
+++ tests/unit/events.spec.js
@@ -2,15 +2,19 @@ import { shallowMount } from '@vue/test-utils'
 import Agile from '@/Agile.vue'
 
 const slides = '<div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div>'
+const options = {
+	propsData: {
+		throttleDelay: 0
+	},
+	slots: {
+		default: slides
+	}
+}
 
 describe('Events emitting:', () => {
 	describe('@after-change', () => {
 		test('should be called after a current slide changes', async () => {
-			const wrapper = shallowMount(Agile, {
-				slots: {
-					default: slides
-				}
-			})
+			const wrapper = shallowMount(Agile, options)
 
 			const nextButton = wrapper.find({ ref: 'nextButton' })
 			await nextButton.trigger('click')
@@ -21,11 +25,7 @@ describe('Events emitting:', () => {
 
 	describe('@before-change', () => {
 		test('should be called before a current slide changes', () => {
-			const wrapper = shallowMount(Agile, {
-				slots: {
-					default: slides
-				}
-			})
+			const wrapper = shallowMount(Agile, options)
 
 			const nextButton = wrapper.find({ ref: 'nextButton' })
 			nextButton.trigger('click')
