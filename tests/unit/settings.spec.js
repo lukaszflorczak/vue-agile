08c3081dbe651a5d493efbcc0ac11ac6a59552b2
Łukasz Florczak <lukasz@florczak.dev>
2020-04-18 10:14:27 +0200

    initialSettings unit test

---
+++ tests/unit/settings.spec.js
@@ -0,0 +1,35 @@
+import { shallowMount } from '@vue/test-utils'
+import Agile from '@/Agile.vue'
+
+
+describe('Settings:', () => {
+	describe('initialSettings object:', () => {
+		test('should contain all possible props without options', async () => {
+			const { vm } = shallowMount(Agile)
+			const { initialSettings, $props } = vm
+
+			Object.keys($props).forEach(key => {
+				const condition = key !== 'options'
+				expect(initialSettings.hasOwnProperty(key)).toEqual(condition)
+			})
+		})
+
+		test('options object should be merged with other props', async () => {
+			const { vm } = shallowMount(Agile, {
+				propsData: {
+					options: {
+						infinite: false,
+						fade: true,
+						throttle: 0
+					}
+				}
+			})
+
+			const { initialSettings, $props } = vm
+
+			expect(initialSettings.infinite).toEqual(false)
+			expect(initialSettings.fade).toEqual(true)
+			expect(initialSettings.throttle).toEqual(0)
+		})
+	})
+})
